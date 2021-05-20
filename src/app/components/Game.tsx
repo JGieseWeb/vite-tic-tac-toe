439103131a5fee92d056c5be1e75b897b072bccc
jannik <giese.jannik@web.de>
2021-05-20 17:15:28 +0200

    :tada: Cretated tic-tac-toe Game
    Cretated tic-tac-toe Game

--- src/app/components/Game.tsx
+++ src/app/components/Game.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import Board from './Board';
+import React from "react";
+import Board from "./Board";
 
 function Game(): JSX.Element {
   return (
@@ -14,5 +14,24 @@ function Game(): JSX.Element {
     </div>
   );
 }
-
 export default Game;
+
+export function calculateWinner(squares: string[]): string | null {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
